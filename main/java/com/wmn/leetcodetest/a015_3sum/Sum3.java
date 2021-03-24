d06052c78670a9a4c7a30a56051d26d0963c8ddb
bywmn <572743026@qq.com>
2021-03-25 00:28:20 +0800

    优化3sum解法

--- main/java/com/wmn/leetcodetest/a015_3sum/Sum3.java
+++ main/java/com/wmn/leetcodetest/a015_3sum/Sum3.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 /**
  * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
- *
+ * <p>
  * 注意：答案中不可以包含重复的三元组。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/3sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -23,31 +23,33 @@ public class Sum3 {
             return list;
         }
         Arrays.sort(nums);
-        if (nums[0] > 0) {
-            return list;
-        }
-        for (int i = 0; i < nums.length - 2; i ++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
             if (i >= 1 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int count = -nums[i];
-            for (int j = i + 1;j <nums.length -1 ;j++) {
-                if (j > i +1 && nums[j] == nums[j -1]) {
+            int count = 0 - nums[i];
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                if (start> i + 1 && nums[start] == nums[start-1]) {
+                    start++;
                     continue;
                 }
-                int end = nums.length -1;
-                while (j <end && nums[j] + nums[end] > count) {
+                if (nums[start] + nums[end] > count) {
                     end--;
-                }
-                if (j == end) {
-                    break;
-                }
-                if (nums[j] + nums[end] == count) {
+                } else if (nums[start] + nums[end] < count) {
+                    start++;
+                } else {
                     List<Integer> temp = new ArrayList<Integer>();
                     temp.add(nums[i]);
-                    temp.add(nums[j]);
+                    temp.add(nums[start]);
                     temp.add(nums[end]);
                     list.add(temp);
+                    start++;
+                    end--;
                 }
             }
         }
